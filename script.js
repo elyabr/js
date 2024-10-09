793c6e0009ce90bc1067e4a8b013ac2bd723ebef
elyabr <jupapinga13@gmail.com>
2024-10-09 16:30:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function HTML() {
+    alert("JavaScript surgiu no contexto de páginas web que eram essencialmente estáticas, escritas apenas em HTML. A ideia era adicionar interatividade, permitindo que os desenvolvedores manipulassem o DOM. Com isso, foi possível criar elementos dinâmicos, como formulários que validam dados antes de serem enviados ou menus que se expandem ao serem clicados. Ao longo dos anos, a compatibilidade de JavaScript com diferentes navegadores evoluiu, levando ao desenvolvimento de padrões como o ECMAScript, que garantiu uma linguagem mais robusta e consistente. A evolução do HTML5 também permitiu que JavaScript interagisse com novos elementos nativos, como vídeos e gráficos, expandindo ainda mais suas capacidades.");
+    }
+
+function CSS(){
+    alert("Com o crescimento do design web, a interação entre JavaScript e CSS se tornou cada vez mais relevante. Inicialmente, a manipulação de estilos era limitada, mas a introdução de propriedades como style no JavaScript permitiu mudanças dinâmicas em tempo real. Isso possibilitou a criação de animações, transições e efeitos de hover que melhoraram significativamente a experiência do usuário. O surgimento de frameworks de animação, como GSAP e bibliotecas como jQuery, facilitou ainda mais essa interação, permitindo que desenvolvedores criassem interfaces ricas e responsivas sem muito esforço.")
+
+
+}
+
+function PHP(){
+    alert("avaScript e PHP sempre tiveram papéis complementares no desenvolvimento web. PHP é uma linguagem do lado do servidor, responsável por gerar conteúdo dinâmico e interagir com bancos de dados, enquanto JavaScript atua no lado do cliente, lidando com a interação do usuário. A introdução do AJAX no início dos anos 2000 revolucionou essa relação, permitindo que o JavaScript enviasse e recebesse dados do servidor sem recarregar a página. Isso significava que aplicações web podiam ser mais rápidas e responsivas, com funcionalidades como atualizações em tempo real, formulários dinâmicos e carregamento de conteúdo sob demanda.")
+
+}
